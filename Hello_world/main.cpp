e0c7ad4583f5672e4e728444fd84bf71d196f885
Hongyu Yao <hongyuy@umich.edu>
2021-07-21 15:31:18 +0800

    Initial Commit

---
+++ Hello_world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello_world
+//
+//  Created by Hongyu Yao on 7/21/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
